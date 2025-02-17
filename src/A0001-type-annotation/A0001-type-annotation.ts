7ce7ee2b5203ddd86cd1c56b10a1a9cfc4f6daf6
Gustavo Gantois <gugagantois@gmail.com>
2025-02-16 21:35:01 -0300

    first commit

---
+++ src/A0001-type-annotation/A0001-type-annotation.ts
@@ -0,0 +1,7 @@
+/*eslint-disable*/
+//Tipos básicos (aqui ocorre a inferência de tipos)
+const name: string = "Gustavo Gantois"; //Qualquer tipo de strings;
+const idade: number = 21; // 10, 1.57, -5.55, 0xf00d, 0b1010, 0o7744;
+const adulto: boolean = true; //true ou false;
+const simbolo: symbol = Symbol("&");//symbol
+const big: bigint = 10n; //bigint
