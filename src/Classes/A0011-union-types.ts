7bd5d5cbcf69a1b57c74ee186378ab9a72e265a0
Gustavo Gantois <gugagantois@gmail.com>
2025-03-02 17:43:38 -0300

    feat:Creating A0011-union-types.ts

---
+++ src/Classes/A0011-union-types.ts
@@ -0,0 +1,12 @@
+function addConcat(a: number | string | boolean, b: number | string | boolean):number | string {
+    if (typeof a === 'number' && typeof b === 'number') return a + b;
+    return `${a}${b}`;
+}
+
+console.log(addConcat(10, 20));
+console.log(addConcat('10', '20'));
+console.log(addConcat(10, '20'));
+console.log(addConcat('10', 20));
+console.log(addConcat(true, true));
+
+//Você utiliza quando você quer um tipo ou outro, ou quantos tipos eu quiser.
